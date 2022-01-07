c8c94a6b259fe99221ac8c84016b1252a75ec1cc
jeannlucas <jeann.lucas@outlook.com>
2022-01-07 16:32:26 -0300

    criar comida para cobrinha

--- script.js
+++ script.js
@@ -2,17 +2,20 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
-
 snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
 
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
-    context.fillStyle = "lightgree";
-    context.fillRect(0,0,16 * box, 16 * box);
+    context.fillStyle = "black";
+    context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha() {
@@ -20,6 +23,11 @@ function criarCobrinha() {
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
@@ -42,6 +50,7 @@ function iniciarJogo() {
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
